6245fe8877ebdad4272274aed8dedd88a09f4899
Sales <contabilgs3@gmail.com>
2024-10-16 17:00:55 -0300

    Componente Main, Desenvolvendo Componente Bancos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login.component';
 import { MainComponent } from './componentes/main/main.component';
+import { BancosComponent } from './componentes/bancos/bancos.component';
 
 export const routes: Routes = [
     {path:'login', component:LoginComponent},
-    {path:'', component: MainComponent}
+    {path:'', component: MainComponent, children:[
+        {path:'bancos', component: BancosComponent}
+    ]}
 ];
--- src/app/componentes/main/main.component.ts
+++ src/app/componentes/main/main.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { SessaoService } from '../../servicos/sessao.service';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+  usuario : string;
 
   constructor(private sessao:SessaoService ){
-    console.log(sessao)
+    this.usuario = this.sessao.nome;
+  }
+
+  navegarAba(abaAtiva : HTMLElement) {
+    let abas = document.querySelectorAll('.aba');
+
+    for(let i = 0; i < abas.length; i++){
+      abas[i]?.classList.remove('aba-ativa');
+    }
+
+    abaAtiva.classList.add('aba-ativa')
   }
 }
